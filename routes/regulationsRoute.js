0c17a8017649dabe4c891342e34c3c69453c542f
uhakdt <uhakdt@gmail.com>
2021-11-06 17:31:02 +0000

    1. Changed to Get request 2. console logging to test.

--- routes/regulationsRoute.js
+++ routes/regulationsRoute.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const request = require('request');
+const crypto = require('crypto');
 
 const router = express.Router();
 
 
 // INPUT REQUEST FROM EBAY FOR ACCOUNT DELETION
-router.post('/api/v1/accountDeletion', async (req, res) => {
+router.get('/api/v1/accountDeletion', async (req, res) => {
   try {
-    console.log(req.body);
+    console.log(req.query.challenge_code);
+    console.log(req.body)
+    console.log(req.params)
+    console.log(req.query)
+
+    // const hash = createHash('sha256');
+    // hash.update(challengeCode);
+    // hash.update(verificationToken);
+    // hash.update(endpoint);
+    // const responseHash = hash.digest('hex');
+    // const challengeResponse = new Buffer.from(responseHash).toString();
+
     res.status(200).json({
-      status: "OK",
-      data: {
-        order: req.body
-      }
+      challengeRsponse: challengeResponse
     })
   } catch (error) {
     console.log(error);
