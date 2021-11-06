53f03590f3458051863d2ed2c4558286bc07906e
uhakdt <uhakdt@gmail.com>
2021-11-06 18:53:21 +0000

    1. Added the necessary requirements for verification of ebay deletion of account section.

--- routes/regulationsRoute.js
+++ routes/regulationsRoute.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const request = require('request');
-const crypto = require('crypto');
-
+const { createHash } = await import('crypto');
 const router = express.Router();
 
 
 // INPUT REQUEST FROM EBAY FOR ACCOUNT DELETION
 router.get('/api/v1/accountDeletion', async (req, res) => {
   try {
-    console.log(req.query.challenge_code);
-    console.log(req.body)
-    console.log(req.params)
-    console.log(req.query)
+    const challengeCode = req.query.challenge_code;
+    const verificationToken = "FFEdv6JHpk1KEuNZma8Tr2910606jYtB";
+    const endpoint = "https://kaientai-ebay.herokuapp.com/api/v1/accountDeletion";
 
-    // const hash = createHash('sha256');
-    // hash.update(challengeCode);
-    // hash.update(verificationToken);
-    // hash.update(endpoint);
-    // const responseHash = hash.digest('hex');
-    // const challengeResponse = new Buffer.from(responseHash).toString();
+    const hash = createHash('sha256');
+    hash.update(challengeCode);
+    hash.update(verificationToken);
+    hash.update(endpoint);
+    const responseHash = hash.digest('hex');
+    const challengeResponse = new Buffer.from(responseHash).toString();
 
     res.status(200).json({
       challengeRsponse: challengeResponse
